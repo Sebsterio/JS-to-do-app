bd04b328280aeec99d1fcdc9a1010ea920fd32f1
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-02 14:57:01 +0000

    add core functionality; add  transitions

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+/*** Core functionality ***/
+
 const form = document.getElementById("addForm");
 const input = form.querySelector("#input");
 const items = document.getElementById("items");
+const filter = document.getElementById("filter");
 
+if (sessionStorage.tasks) {
+  items.innerHTML = sessionStorage.getItem("tasks");
+}
+
+// create <li> item from #addForm value
 function addItem(e) {
   e.preventDefault();
   const itemText = input.value;
@@ -9,33 +17,74 @@ function addItem(e) {
 
   const newItem = document.createElement("li");
   newItem.classList.add("list-group-item");
-  newItem.innerHTML = `
-    ${itemText}
-    <button class="btn btn-primary btn-sm float-right delete">&#10003</button>
-  `;
+  newItem.classList.add("custom-list-group-item");
+  newItem.innerHTML = `${itemText}
+    <button class="btn btn-primary btn-sm float-right complete">&#10003</button>`;
+
   items.appendChild(newItem);
+  sessionStorage.setItem("tasks", items.innerHTML);
+
   input.value = "";
+  input.focus();
 }
 
-function removeItem(e) {
-  if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove(); // no IE support
+// remove completed item -- called from completeItem()
+function handleTransitionEnd(e) {
+  if (e.target.tagName == "LI" && e.propertyName == "opacity") {
+    e.target.parentElement.removeChild(e.target);
+    sessionStorage.setItem("tasks", items.innerHTML);
   }
 }
 
+// remove <li> - called by `complete` button click
+function completeItem(e) {
+  if (e.target.classList.contains("complete")) {
+    e.target.parentElement.style["text-decoration"] = "line-through";
+
+    setTimeout(function() {
+      e.target.parentElement.style.opacity = 0;
+      // followed by handleTransitionEnd()
+    }, 150);
+  }
+}
+
+// filter <ul> items with #search input value
+function filterItems(e) {
+  let firstIsAssigned = false;
+  items.querySelectorAll("li").forEach(item => {
+    //
+    if (item.innerText.includes(e.target.value)) {
+      item.style.display = "block";
+
+      // set border-top of first visible <li>
+      if (!firstIsAssigned) {
+        item.classList.add("item--first-visible");
+        firstIsAssigned = true;
+      } else {
+        item.classList.remove("item--first-visible");
+      }
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 form.addEventListener("submit", addItem);
-items.addEventListener("click", removeItem);
+items.addEventListener("click", completeItem);
+items.addEventListener("transitionend", handleTransitionEnd);
+filter.addEventListener("keyup", filterItems);
 
-// moving gradient bg
+//
+//
+/***  Rotating bg ***/
 
-// let angle = 0;
+let angle = getComputedStyle(document.documentElement).getPropertyValue(
+  "--angle"
+);
+function newFunction() {
+  document.documentElement.style.setProperty("--angle", `${angle}deg`);
+  angle++;
+  if (angle >= 360) angle = angle - 360;
+}
 
-// function newFunction() {
-//   const gradient = `linear-gradient(${angle}deg, #16a085, #f4d03f)`;
-//   document.body.style.background = gradient;
-//   angle++;
-//   if (angle >= 360) angle = angle - 360;
-//   console.log(angle);
-// }
-
-// setInterval(newFunction, 80);
+setInterval(newFunction, 50);
--- readme.md
+++ readme.md
@@ -2,15 +2,9 @@
 
 ### Practice in DOM manipulation
 
-HTML Structure and style loosely based on Brad Traversy's tutorial project:
+HTML Structure based on Brad Traversy's tutorial project:
 https://codepen.io/bradtraversy/pen/Bwapow?editors=1010
 
 Core functionality reverse-engineered from said project.
 
-### to do
-
-- filter
-- overflow
-- background
-- striketrhough transition
-- add/remove item transition
+Added transitions with CSS + JS
